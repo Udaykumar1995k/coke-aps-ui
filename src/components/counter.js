8b56d2eba31065328540adf88efa2cf85f2d4c2e
uday kammari <uday.kammari@infosys.com>
2024-07-02 15:02:29 +0530

    message: Format Code

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,26 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { decrement, increment } from '../redux/actions/counterSlice';
 
-const Counter=()=>{
-    const count = useSelector((state) => state.counter.value)
-    const dispatch = useDispatch()
-    return(
-        <div>
-             <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>  {count}  </span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-        </div>
-    )
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>  {count}  </span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+    </div>
+  )
 }
 
 export default Counter;
