333a400aeb60f023295fd2f628e4e4a40ce71ba4
Trupti Mache <trupti.mache@infosys.com>
2024-07-09 15:28:31 +0530

    Changes in dropdown component

--- src/components/common/Dropdown.js
+++ src/components/common/Dropdown.js
@@ -3,44 +3,34 @@ import { FormControl, InputLabel, Select , MenuItem } from '@mui/material';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import { useTheme } from '@mui/material/styles';
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 5;
-
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
-function getStyles(options, value, theme) {
-    return {
-      fontWeight:
-      value.indexOf(options) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
-    };
-  }
-
-
-const Dropdown = ({label,value, options=[], handleChange }) => {
+const Dropdown = ({label,value, options=[], handleChange, ...props }) => {
     const theme = useTheme();
-
     return (
-         <FormControl sx={{ m: 1, width: 250, height: 'auto', '.MuiInputBase-root': { height: 49 } }}>
-            <InputLabel id="dropdown-label">{label}</InputLabel>
+         <FormControl  fullWidth  sx={{
+         width: '100%',
+          [theme.breakpoints.down('md')]: {
+            width: '40%', 
+          },
+          [theme.breakpoints.up('md')]: {
+            width: '40%',
+          },
+        }} >
+            <InputLabel id={props.inputLabelId || "dropdown-label"}>{label}</InputLabel>
            < Select
-                id="dropdown"
-                labelId="dropdown-label"
+                id={props.id ||'dropdown'}
+                labelId={props.labelId ||'dropdown-label'}
                 value={value}
                 onChange={handleChange}
-                input={<OutlinedInput label={label} />}
-                MenuProps={MenuProps}  
+                input={<OutlinedInput label={label} sx={{ height:'48px', minHeight: '48px', 
+                 }}/>}
+                 
+                sx={{
+                  height: props.height ||'48px', 
+                  minHeight: props.minHeight || '48px', 
+                }} 
             >
                 {options.map((option) => (
-                    <MenuItem key={option.value} value={option.value}  style={getStyles(option, value, theme)}>
+                    <MenuItem key={option.value} value={option.value}>
                        {option.label}
                     </MenuItem>
                 ))}
