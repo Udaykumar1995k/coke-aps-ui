6ef2178b01dceca7a60879c777374df117795277
Trupti Mache <trupti.mache@infosys.com>
2024-07-10 14:05:22 +0530

    Changes css of dropdown component

--- src/components/common/Dropdown.js
+++ src/components/common/Dropdown.js
@@ -1,36 +1,24 @@
 import React from 'react';
 import { FormControl, InputLabel, Select , MenuItem } from '@mui/material';
 import OutlinedInput from '@mui/material/OutlinedInput';
-import { useTheme } from '@mui/material/styles';
 
 const Dropdown = ({label,value, options=[], handleChange, ...props }) => {
-    const theme = useTheme();
     return (
-         <FormControl  fullWidth  sx={{
-         width: '100%',
-          [theme.breakpoints.down('md')]: {
-            width: '40%', 
-          },
-          [theme.breakpoints.up('md')]: {
-            width: '40%',
-          },
-        }} >
+         <FormControl  style={{width:props.width||'50%'}}> 
             <InputLabel id={props.inputLabelId || "dropdown-label"}>{label}</InputLabel>
            < Select
                 id={props.id ||'dropdown'}
                 labelId={props.labelId ||'dropdown-label'}
                 value={value}
                 onChange={handleChange}
-                input={<OutlinedInput label={label} sx={{ height:'48px', minHeight: '48px', 
-                 }}/>}
-                 
+                input={<OutlinedInput label={label} sx={{ height: props.height||'48px', minHeight: props.height||'48px'}}/>} 
                 sx={{
                   height: props.height ||'48px', 
                   minHeight: props.minHeight || '48px', 
                 }} 
             >
                 {options.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
+                    <MenuItem key={option.value} value={option.value} >
                        {option.label}
                     </MenuItem>
                 ))}
