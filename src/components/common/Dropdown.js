52aa515a9c5f8c0010c168ebcab664b5bfc4ee18
Trupti Mache <trupti.mache@infosys.com>
2024-07-12 11:44:43 +0530

    added multiple prop to dropdown

--- src/components/common/Dropdown.js
+++ src/components/common/Dropdown.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import { FormControl, InputLabel, Select , MenuItem } from '@mui/material';
+import React, { useState } from 'react';
+import { FormControl, InputLabel, Select, MenuItem} from '@mui/material';
 import OutlinedInput from '@mui/material/OutlinedInput';
 
-const Dropdown = ({label,value, options=[], handleChange, ...props }) => {
+const Dropdown = ({ label, value, options = [], handleChange, ...props }) => {
+
+    const selectProps = {
+        id: props.id || 'dropdown',
+        labelId: props.labelId || 'dropdown-label',
+        value: value,
+        onChange: handleChange,
+        input: <OutlinedInput label={label} sx={{ height: props.height || '48px', minHeight: props.height || '48px' }} />,
+        sx: {
+            height: props.height || '48px',
+            minHeight: props.minHeight || '48px',
+        },
+        multiple: props.multiple,
+    };
+
+    if (props.multiple) {
+        selectProps.renderValue = (selected) => selected.join(', ');
+    }
+
     return (
-         <FormControl  style={{width:props.width||'50%'}}> 
+        <FormControl sx={{ width: props.width || '50%' }}>
             <InputLabel id={props.inputLabelId || "dropdown-label"}>{label}</InputLabel>
-           < Select
-                id={props.id ||'dropdown'}
-                labelId={props.labelId ||'dropdown-label'}
-                value={value}
-                onChange={handleChange}
-                input={<OutlinedInput label={label} sx={{ height: props.height||'48px', minHeight: props.height||'48px'}}/>} 
-                sx={{
-                  height: props.height ||'48px', 
-                  minHeight: props.minHeight || '48px', 
-                }} 
-            >
+            < Select {...selectProps}>
                 {options.map((option) => (
                     <MenuItem key={option.value} value={option.value} >
-                       {option.label}
+                        {option.label}
                     </MenuItem>
                 ))}
             </Select>
