7354de4bf5fdc7bcb725275ce6e84a3ce7f252cd
sandeepshivarathri <shivarathri.sandeep@ifosys.com>
2024-07-26 12:36:21 +0530

    fix: fixed the issues

--- src/pages/analyticsresults/AnalyticsResultsTab.js
+++ src/pages/analyticsresults/AnalyticsResultsTab.js
@@ -2,20 +2,27 @@ import './AnalyticsResults.css'
 
 const AnalyticsResultsTab = (props) =>{
     return (
-        <div className="analytic-results-tab">
-      <div className="analytic-results">
-      <div className="result-item">
-          <p className="analytics-text-color">Period Consider for Analytics</p>
-          <p className="text-color">{props?.periodOfResults}</p>
-      </div>
-      <div className="result-item">
-          <p className="analytics-text-color">Analytics Initiated by</p>
-          <p className="text-color">{props?.initatedBy}</p>
-      </div>
-      </div>
-      <div className="input-details">
-        <p><a style={{color:"black"}}href={props?.link || "#"}>Click Here</a> to View the Complete Input Details</p>
-      </div >
+    <div className="analytic-results-tab">
+        <div className="analytic-results">
+            <div className="result-item">
+                <span className="analytics-text">Period Consider for Analytics</span>
+                <p >01/January/23 to 31/December/23</p>
+            </div>
+            <div className="result-item">
+                <span className="analytics-text">Analytics Initiated by</span>
+                <p >Administrator(Schedule Based)</p>
+            </div>
+            <div className="result-item">
+                <span className="analytics-text">Analytics Run Unique ID</span>
+                <p >LT_ADMIN_FY24_Apr_0010</p>
+            </div>
+           
+        </div>
+        <div className="input-details">
+            <p>
+                <a style={{color:"black"}}href={props?.link || "#"}>Click Here</a> to View the Complete Input Details
+            </p>
+        </div >
     </div>
 
     );
