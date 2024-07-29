0258d425a93f4302c3a386037b6c60f053a3a711
sandeepshivarathri <shivarathri.sandeep@ifosys.com>
2024-07-29 09:26:43 +0530

    fix: fixed the issues

--- src/pages/ActionDashboard/actionDashboard.js
+++ src/pages/ActionDashboard/actionDashboard.js
@@ -1,6 +1,6 @@
 import Cards from "../../components/common/card/Card";
 const ActionDashboard =()=>{
-    const cardData =[{title:"High LT Variation"},{title:"Medium LT Variation"},{title:"Low LT Variation"}]
+    const cardData =[{title:"High LT Variation", description:"Materials - Supplier - Plant"},{title:"Medium LT Variation"},{title:"Low LT Variation"}]
     return(
         <div>
             <Cards cards={cardData} maxWidth="1050px"/>
