41eb2f7cd12d6ea828422fff6337834249bb0439
sandeepshivarathri <shivarathri.sandeep@ifosys.com>
2024-08-01 18:28:59 +0530

    fix: fixed the issues

--- src/pages/analyticsresults/AnalyticsResultsTab.js
+++ src/pages/analyticsresults/AnalyticsResultsTab.js
@@ -1,7 +1,24 @@
 import './AnalyticsResults.css'
 import ActionInput from '../../components/common/actioninput/ActionInputField';
 import ButtonComponent from '../../components/common/button/Button';
+import TabSection from '../../components/common/TabSection';
+import Dropdown from '../../components/common/Dropdown';
+import { useState } from 'react';
 const AnalyticsResultsTab = (props) =>{
+    const [value, setValue] = useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+      };
+    const [formData, setFormData] = useState({
+        supplier: '',
+        material: '',
+        shipFrom: '',
+        shipTo: ''
+    });
+    const handleFormData = (event) =>{
+        setFormData({...formData,[event.target.name]: event.target.value})
+
+    }
     return (
     <div>
         <div className="analytic-results-tab">
@@ -25,6 +42,120 @@ const AnalyticsResultsTab = (props) =>{
                     <a style={{color:"black"}} href={props?.link || "#"}>Click Here</a> to View the Complete Input Details
                 </p>
             </div >
+        </div>
+        <div className="analytic-results material-tab-container border-radius-container">
+            <div style={{width:"75%"}}>  
+                <TabSection  style={{ maxWidth:"450px", marginBottom:"30px", width:"100%"}}  label={["Regular Items", "Advance PO Items"]} value={value} handleChange={handleChange} count={[12, 999]} />
+                <div className="analytic-results">
+                    <div  className="flex-direction-column">
+                        <div className="material-tab-container analytic-results-tab material-items-container">
+                            <div  className="result-item material-items">
+                                <label>Supplier</label>
+                                    <Dropdown 
+                                        width="200px"
+                                        label="Select Supplier"
+                                        value={formData.supplier}
+                                        name="supplier"
+                                        options={[
+                                            {value: 'ireland', label: 'CPS Balina'},
+                                            {value: 'india',label:'CPS Hyderabad'}
+                                        ]}
+                                        handleChange={handleFormData}
+                                    />
+                                <label>Material</label>
+                                    <Dropdown 
+                                        width="200px"
+                                        label="Select Material"
+                                        value={formData.material}
+                                        name="material"
+                                        options={[
+                                            {value: 'Material1', label: 'Material1'},
+                                            {value: 'Material2',label:'Material2'}
+                                            ]}
+                                        handleChange={handleFormData}
+                                    />
+                                <label>Ship From</label>
+                                    <Dropdown 
+                                        width="200px"
+                                        label="Select Ship From"
+                                        value={formData.shipFrom}
+                                        name="shipFrom"
+                                        options={[
+                                            {value: 'Location1', label: 'India'},
+                                            {value: 'Location2',label:'Ireland'}
+                                            ]}
+                                        handleChange={handleFormData}
+                                    />
+                                <label>Ship To</label>
+                                    <Dropdown 
+                                        width="200px"
+                                        label="Select Ship To"
+                                        value={formData.shipTo}
+                                        name="shipTo"
+                                        options={[
+                                            {value: 'Location1', label: 'India'},
+                                            {value: 'Location2',label:'Ireland'}
+                                            ]}
+                                        handleChange={handleFormData}
+                                    />
+                                <div style={{ display:"flex", justifyContent:"center"}}>
+                                    <ButtonComponent type="secondary" label="Ok" maxWidth="80px"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div  style={{paddingLeft:"12px"}}className="analytic-results result-margin">
+                            <p>Variance (Actual vs</p> 
+                            <Dropdown
+                                width="230px"
+                                options={[
+                                    {value: 'Median', label: 'Median'},
+                                    {value: 'Mean',label:'Mean'},
+                                    {value: 'Quartiles',label:'Quartiles'},
+                                    {value: '75th Percentiles',label:'75th Percentiles'},
+                                    {value: '80th Percentiles',label:'80th Percentiles'},
+                                    {value: '90th Percentiles',label:'95th Percentiles'}
+                                    ]}
+                            />
+                            <p>10%</p>
+                        </div>
+                    </div>
+                       <div>
+                            <Dropdown 
+                                width="220px"
+                                options={[
+                                    {value: 'In Last 1 Year', label: 'In Last 1 Year'},
+                                    {value: 'In Last 2 Year',label:'In Last 2 Year'}
+                                    ]}
+                            />
+                        </div>
+                </div>
+            </div>
+            <div className="analytic-results-tab result-margin" style={{width:"25%"}}>
+                <div className="analytics-text">
+                <p>Mean </p>
+                <p>Median </p>
+                <p>Quartiles </p>
+                <p>75th Percentiles </p>
+                <p>80th Percentiles </p>
+                <p>90th Percentiles </p>
+                <p>COV</p>
+                </div>
+                <h5>Recommondation:</h5>
+                <div className="analytic-results recomonded-values-group">
+                    <span>Recomonded values:</span>
+                    <span className="text-alignment">57-60</span>
+                </div>
+                <div className="analytic-results recomonded-values-group">
+                    <span>Confidence Level:</span>
+                    <span className="text-alignment">High Likely(70%)</span>
+                </div>
+                <div className="analytic-results recomonded-values-group">
+                    <span>Reccomondation Based on</span>
+                    <span className="text-alignment">Advance Pos (60% of Total Pos)</span>
+                </div>
+
+            </div>
+            
         </div>
         <div className="analytic-results-tab result-margin">
             <div className="analytic-results">
