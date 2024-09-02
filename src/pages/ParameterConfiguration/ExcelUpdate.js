4472c84dbe723167868d7b3d32ddca2e671ccafc
sandeepshivarathri <shivarathri.sandeep@ifosys.com>
2024-09-02 17:20:16 +0530

    fix: added the excel icon and the delete icon

--- src/pages/ParameterConfiguration/ExcelUpdate.js
+++ src/pages/ParameterConfiguration/ExcelUpdate.js
@@ -4,12 +4,19 @@ import {saveAs} from 'file-saver'
 import './index.css'
 import DownloadIcon from '@mui/icons-material/Download';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
+import ExcelLogo from '../../assets/272697_excel_icon.png'
+import { IconButton } from '@mui/material';
+import { Delete } from '@mui/icons-material';
 const ExcelUpdate = () => {
      const [data, setData] = useState(null);
+     const [fileName, setFileName] = useState('');
     const handleButtonClick = () =>{
         document.getElementById('file_input').click()  
     }
-
+    const handeDeleteIcon = () =>{
+        setData(null);
+        setFileName('');
+    }
     const handleFileUpload = (event) =>{
         const file = event.target.files[0];
         const reader = new FileReader();
@@ -20,6 +27,7 @@ const ExcelUpdate = () => {
             const sheet = workbook.Sheets[sheetName];
             const sheetData = XLSX.utils.sheet_to_json(sheet);
             setData(sheetData);
+            setFileName(file.name)
         };
         reader.readAsArrayBuffer(file);
     }
@@ -46,6 +54,16 @@ const ExcelUpdate = () => {
                 <button className="download-button" id="file_input_button" onClick={handleButtonClick}><UploadFileIcon/>Choose File</button>
                 <p className="font-size">File Should be in '.csv' format</p>
                 <p className="font-size">Max file size that can be upload in 5mb</p>
+                {data && (
+                    <div className="file-container">
+                    <img style={{paddingTop:"10px"}} src={ExcelLogo} alt='logo' width={20} height={20}/>
+                    <span className="font-size" style={{paddingTop:"10px"}}>{fileName}</span>
+                    <IconButton onClick={handeDeleteIcon}>
+                        <Delete/>
+                    </IconButton>
+                </div>
+                )}
+                
             </div>
         </>
     )
