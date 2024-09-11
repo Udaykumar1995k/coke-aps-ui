36a1596882ced1d84dc4c4f67c010f82a94ac65d
sandeepshivarathri <shivarathri.sandeep@ifosys.com>
2024-09-11 11:59:44 +0530

    fix: fixed the issues

--- src/components/common/Searchwithoption/Search.js
+++ src/components/common/Searchwithoption/Search.js
@@ -1,17 +1,27 @@
+import React,{useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const SearchWithOptions = ({ data, width, onChange,clearInputValue,onInputChange }) => {
+const SearchWithOptions = ({ data, width, onChange,clearInputValue,onInputChange,handleClear,name }) => {
+    const [previousValue, setPreviousValue] = useState('');
     return (
         <div className='searchOption' style={{ width: width || '200px' }}>
             <Autocomplete
                 freeSolo
                 id="searchfield"
+                name={name}
                 disableClearable
                 {...clearInputValue ? {} : { value: "" }}
                 options={data.map((option) => option.ApproverName)}
-                onChange={(event, value) =>  onChange(value)}
-                onInputChange={(event, value) =>onInputChange(value)}
+                onChange={(event, value) =>  onChange(value,name)}
+                onInputChange={(event, newValue) => {
+                    if (newValue === '') {
+                      handleClear(previousValue,name); // call the handleClear function with the previous value
+                    } else {
+                      setPreviousValue(newValue)
+                      onInputChange(newValue);
+                    }
+                  }}
                 renderInput={(params) => (
                     <TextField
                         {...params}
--- src/pages/WorkflowManagement/ApprovalCard.js
+++ src/pages/WorkflowManagement/ApprovalCard.js
@@ -6,15 +6,16 @@ import SearchWithOptions from '../../components/common/Searchwithoption/Search';
 import './index.css'
 import ButtonComponent from '../../components/common/button/Button';
 import { IconButton } from '@mui/material';
-
 const ApprovalCard = ({ checkBoxCount, isClicked,setApproval }) => {
   var disabled = false;
   const [disableApprover, setDisableApprover] = useState(true);
   const [approvalCardsData, setApprovalCardsData] = useState([
-    { name: "Level1", label: "Level 1" },
+    { name: "Level1", label: "Level 1", EmailID:"",ApproverName:""},
   ]);
-  const [selectedApprover, setSelectedApprover] = useState({ ApproverName: '', EmailID: '' });
-  const [comment, setComment] = useState("");
+  const [formData, setFormData] = useState({
+    ApproverData: [],
+    Comments: ""
+  })
   const data = [
     { ApproverName: 'Jerry Mathew', EmailID: 'jerry@cococola.com' },
     { ApproverName: 'Jerry George', EmailID: 'george@cococola.com' },
@@ -27,6 +28,8 @@ const ApprovalCard = ({ checkBoxCount, isClicked,setApproval }) => {
       const newApprover = {
         name: `Level${approvalCardsData.length + 1}`,
         label: `Level ${approvalCardsData.length + 1}`,
+        EmailID: "",
+        ApproverName:""
       };
       setApprovalCardsData([...approvalCardsData, newApprover]);
     }
@@ -34,36 +37,66 @@ const ApprovalCard = ({ checkBoxCount, isClicked,setApproval }) => {
   const onHandleDeleteIcon = (index) =>{
     const newData = approvalCardsData.filter((data,i) => i!==index && data)
     setApprovalCardsData(newData);
+    const newFormData = formData.ApproverData.filter((data,i) => i!==index && data)
+    setFormData({...formData,ApproverData:newFormData})
   };
   const styles = approvalCardsData.length === 1 ? (disabled=true,{ cursor:"not-allowed", opacity:0.5 }) : (disabled=false,{ cursor:"pointer"});
   useEffect(()=>{
     if(isClicked){
       setApprovalCardsData([
-        { name: "Level1", label: "Level 1" },
+        { name: "Level1", label: "Level 1", EmailID:"",ApproverName:""},
       ])
+      setDisableApprover(true)
+      setFormData({
+        ApproverData: [],
+        Comments: ""})
       setApproval()
-      setSelectedApprover({ ApproverName: '', EmailID: '' })
-      setComment("")
+      
     }
   },[isClicked])
-    const handleApproverChange = (approverName) => {
+    const handleApproverChange = (approverName,name) => {
       const approver = data.find(item => item.ApproverName === approverName);
       if (approver) {
-        setSelectedApprover(approver);
-        setDisableApprover(false);
+        setFormData({...formData,ApproverData:[...formData.ApproverData,approver]})
+        approvalCardsData.forEach((item) =>{
+          if(item.name === name){
+            item.EmailID = approver["EmailID"]
+            item.ApproverName = approver["ApproverName"]
+          }
+        })
+        setDisableApprover(false)  
       }
   };
   const handleInputChange = (newValue) =>{
-    if(!newValue){
-      setSelectedApprover({ ApproverName: '', EmailID: '' })
-      setDisableApprover(true)
+    if(!newValue){      
+     
     }
+  }
+  const handleClear = (previousValue,name) => {
+      const newFormData = formData.ApproverData.filter((option) =>option.ApproverName !== previousValue)
+      setFormData({...formData,ApproverData: newFormData})
+      approvalCardsData.forEach((item) =>{
+        if(item.name === name){
+          item.EmailID = ""
+          item.ApproverName = ""
+        }
+      })
+      if(newFormData.length === 0) {
+        setDisableApprover(true)
+      
+      }
+  };
+  const addComment = (event) =>{
+    setFormData({...formData,Comments:event.target.value})
   }
   return (
     <>
-    <div className='approval-items-wrapper'>
-        <p style={{ marginTop: '8px', fontSize: '14px' }}>No.of Line Items for Approval
-        </p><ActionInput   props={{ height: "30px", maxWidth: "70px", value:checkBoxCount, disabled:true }} />
+      <div className='approval-items-wrapper'>
+        <p style={{ marginTop: '8px', fontSize: '14px' }}>No.of Line Items for Approval</p>
+        <ActionInput   
+          props={{ height: "30px", maxWidth: "70px", value:checkBoxCount, 
+          disabled:true }} 
+        />
       </div>
       <div className='approval-wrapper'>
         <div className='approver-header'>
@@ -75,17 +108,28 @@ const ApprovalCard = ({ checkBoxCount, isClicked,setApproval }) => {
         {approvalCardsData.map((approver,index) =>(
         <div className='approver-container'>
           <div  style={{paddingTop:"16px", paddingBottom:"10px", paddingLeft:"10px"}} className='level-width'>{approver.label}</div>
-          <div  style={{paddingTop:"10px", paddingBottom:"10px", paddingLeft:"10px"}} className='approver-label-width'>
-            <SearchWithOptions data={data} width="200px" onChange={handleApproverChange} onInputChange ={handleInputChange}clearInputValue={isClicked}/>
-          </div>
-          <div className='email-lable-width'  style={{paddingTop:"10px", paddingBottom:"10px", paddingLeft:"22px"}}>
-            <ActionInput props={{ height: "40px", maxWidth: "220px", value: selectedApprover.EmailID }} placeholder='Email' />
-          </div>
-          <span style={{ paddingTop:"10px", ...styles,  textAlign:"right"}}>
-            <IconButton disabled={disabled}>
-            <DeleteIcon   onClick={()=>onHandleDeleteIcon(index)} />
-            </IconButton>
-          </span>
+            <div  style={{paddingTop:"10px", paddingBottom:"10px", paddingLeft:"10px"}} className='approver-label-width'>
+              <SearchWithOptions 
+                data={data} 
+                width="200px" 
+                onChange={handleApproverChange} 
+                onInputChange ={handleInputChange} 
+                clearInputValue={isClicked}
+                handleClear={handleClear}
+                name={approver.name}
+              />
+            </div>
+            <div className='email-lable-width'  style={{paddingTop:"10px", paddingBottom:"10px", paddingLeft:"22px"}}>
+              <ActionInput 
+                props={{ height: "40px", maxWidth: "220px", value:approver.EmailID }} 
+                placeholder='Email' 
+              />
+            </div>
+            <span style={{ paddingTop:"10px", ...styles,  textAlign:"right"}}>
+              <IconButton disabled={disabled}>
+                <DeleteIcon   onClick={()=>onHandleDeleteIcon(index)} />
+              </IconButton>
+            </span>
         </div>
         ))}
       </div>
@@ -96,12 +140,17 @@ const ApprovalCard = ({ checkBoxCount, isClicked,setApproval }) => {
           type="primary"
           disabled={disableApprover}
           onClick={addApprover}
-        /></div>
-      <div><label style={{ fontSize: '14px' }}>Comments</label> <ActionInput  onChange={(e)=>setComment(e.target.value)}props={{ height: "65px", maxWidth: "400px" ,value:comment }} placeholder='Kindly check the Lead time and approve.' /></div>
+        />
+      </div>
+      <div>
+        <label style={{ fontSize: '14px' }}>Comments</label> 
+        <ActionInput  
+          onChange={addComment} 
+          props={{ height: "65px", maxWidth: "400px" ,value:formData.Comments, name:"Comments" }} 
+          placeholder='Kindly check the Lead time and approve.' 
+        />
+      </div>
    </>
-
-
-
   )
 }
 
